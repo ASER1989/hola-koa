a5ed049e02a6afd24658c3861ae39b4db40620e6
ASER1989 <aser1989@sina.com>
2025-06-06 07:14:01 +0000

    remove unused code

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,15 +1,9 @@
 import Koa from 'koa';
-import Router from '@koa/router';
 import bodyParser from 'koa-bodyparser';
 import { useResponseFormatter } from './middlewares/responseFormatter';
 import helloRouters from './routes/hello';
 
 const app = new Koa();
-const router = new Router();
-
-router.get('/', (ctx) => {
-  ctx.body = 'Hello Koa TypeScript Scaffold!';
-});
 
 app
   .use(
